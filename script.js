a2d449cb1a72f44c296611bc25beea7d708ea8f4
Sidharth K V <sidharthkv1100@gmail.com>
2025-12-26 17:40:12 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const tracks = [
+    { name: "Enthinee Mizhi", src: "audio/enthinee_mizhi.mp3" },
+    { name: "Devakanyaka", src: "audio/devakanyaka.mp3" },
+    { name: "Etho Saayaana", src: "audio/etho_saayaana.mp3" },
+    { name: "Kaana Chembaka Poo", src: "audio/kaana_chembaka_poo.mp3" },
+    { name: "Kanmanipoove", src: "audio/kanmanipoove.mp3" }
+];
+
+let currentTrackIndex = 0;
+let isPlaying = false;
